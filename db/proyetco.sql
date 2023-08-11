466fe4a0efe4ab75cf5a6c3fe3912fd78abec021
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-08-10 23:07:35 -0600

    commit

---
+++ db/proyetco.sql
@@ -0,0 +1,104 @@
+-- MariaDB dump 10.19  Distrib 10.5.19-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: Elvis
+-- ------------------------------------------------------
+-- Server version	10.5.19-MariaDB-0+deb11u2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Horario`
+--
+
+DROP TABLE IF EXISTS `Horario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Horario` (
+  `ID` int(11) NOT NULL,
+  `hora_inicio` time DEFAULT NULL,
+  `hora_final` time DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Horario`
+--
+
+LOCK TABLES `Horario` WRITE;
+/*!40000 ALTER TABLE `Horario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Horario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Laboratorio`
+--
+
+DROP TABLE IF EXISTS `Laboratorio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Laboratorio` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Laboratorio`
+--
+
+LOCK TABLES `Laboratorio` WRITE;
+/*!40000 ALTER TABLE `Laboratorio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Laboratorio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Reserva`
+--
+
+DROP TABLE IF EXISTS `Reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Reserva` (
+  `ID` int(11) NOT NULL,
+  `id_Horario` int(11) DEFAULT NULL,
+  `id_Usuario` int(11) DEFAULT NULL,
+  `id_Laboratorio` int(11) DEFAULT NULL,
+  `Hora` time DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `id_Laboratorio` (`id_Laboratorio`),
+  KEY `id_Horario` (`id_Horario`),
+  CONSTRAINT `Reserva_ibfk_1` FOREIGN KEY (`id_Laboratorio`) REFERENCES `Laboratorio` (`ID`),
+  CONSTRAINT `Reserva_ibfk_2` FOREIGN KEY (`id_Horario`) REFERENCES `Horario` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reserva`
+--
+
+LOCK TABLES `Reserva` WRITE;
+/*!40000 ALTER TABLE `Reserva` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-09 21:32:01
